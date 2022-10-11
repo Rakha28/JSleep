f959c6fec34a1da053e2a119d959d4807f51dee8
Rakha28 <rakha.argya@ui.ac.id>
2022-10-11 23:40:58 +0700

    CS Modul 5 - revisi Algorithm

--- src/RakhaArgyaZahranJSleepDN/Algorithm.java
+++ src/RakhaArgyaZahranJSleepDN/Algorithm.java
@@ -6,58 +6,31 @@ public class Algorithm {
     private Algorithm(){
     }
 
-    public static <T> int count(Iterator<T> iterator, T value){
-        int count = 0;
-        while(iterator.hasNext()){
-            if(iterator.next().equals(value)){
-                count++;
-            }
-        }
-        return count;
-    }
     public static <T> int count(T[] array, T value){
-        int count = 0;
-        for(T t : array){
-            if(t.equals(value)){
-                count++;
-            }
-        }
-        return count;
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return count(it, value);
     }
-    public static <T> int count(Iterable<T> iterable, Predicate<T> predicate){
-        int count = 0;
-        for(T t : iterable){
-            if(predicate.test(t)){
-                count++;
-            }
-        }
-        return count;
+    public static <T> int count(Iterable<T> iterable, T value){
+        final Iterator<T> it = iterable.iterator();
+        return count(it, value);
     }
-
-    public static <T> int count(T[] array, Predicate<T> predicate){
-        int count = 0;
-        for(T t : array){
-            if(predicate.test(t)){
-                count++;
-            }
-        }
-        return count;
+    public static <T> int count(Iterator<T> iterator, T value){
+        final Predicate<T> pred = value::equals;
+        return count(iterator, pred);
     }
-
-    public static <T> int count(Iterator<T> iterator, Predicate<T> predicate){
+    public static <T> int count(T [] array, Predicate<T> pred){
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return count(it, pred);
+    }
+    public static <T> int count(Iterable<T> iterable, Predicate<T> pred){
+        final Iterator<T> it = iterable.iterator();
+        return count(it, pred);
+    }
+    public static <T> int count(Iterator<T> iterator, Predicate<T> pred){
         int count = 0;
         while(iterator.hasNext()){
-            if(predicate.test(iterator.next())){
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public static <T> int count(Iterable<T> iterable, T value){
-        int count = 0;
-        for(T t : iterable){
-            if(t.equals(value)){
+            T current = iterator.next();
+            if(pred.test(current)){
                 count++;
             }
         }
